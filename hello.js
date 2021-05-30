07f32a2f9a357e68aede119d5418055d871d01d4
JONGHEON <kjhijhz@naver.com>
2021-05-30 14:13:35 +0900

    study 3days

---
+++ hello.js
@@ -0,0 +1,25 @@
+/* String
+const what="apple";
+*/
+/*Boolean
+const wat = true;
+*/
+
+/*Number
+const wat = 777;
+*/
+
+
+/*Float
+const wat =55.1;
+*/
+
+const appleInfo =
+ {name:"apple",
+  age:33,
+  gender:"Male",
+  adress:"Seoul"};
+
+
+
+console.log(appleInfo.adress);
